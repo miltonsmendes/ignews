9bce426cbde848aec4a2f331485e1932e0de1bfc
miltonsmendes <miltonsergiom@gmail.com>
2021-06-07 15:34:03 -0300

    Initial commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,32 @@
+import { query as q } from 'faunadb';
+
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
+    providers: [
+        Providers.GitHub({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            scope: 'read:user'
+        }),
+    ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user
+
+            await fauna.query(
+                q.Create(
+                    q.Collection('users'),
+                    { data: { email } }
+                )
+            )
+
+
+
+            return true
+        }
+    }
+})
